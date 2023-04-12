c6eae95f41a0fbf3165de651190fe72bb606c9d0
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-04-12 18:32:11 +0200

    Added a drawing board and made CSS changes

--- src/drawInputFrame.jsx
+++ src/drawInputFrame.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import './assets/css/drawInputFrame.css';
+import Button from '@mui/material/Button';
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+const styles = {
+    border: '0.0625rem solid #9c9c9c'
+};
 
 function DrawInputFrame() {
+    const canvasRef = useRef(null);
+
     return ( 
-        <div>
-            <h1>Draw Input</h1>
+        <div id="drawContainer">
+            <h1>Awaken your imagination !</h1>
+
+            <ReactSketchCanvas
+                style={styles}
+                width="80vw"
+                height="80vh"
+                strokeWidth={4}
+                strokeColor="black"
+                ref={canvasRef}
+            />
+
+            <div id="drawButtonsContainer">
+                <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => {
+                        canvasRef.current
+                            .exportImage("png")
+                            .then(data => {
+                                console.log(data);
+                            })
+                            .catch(e => {
+                                console.log(e);
+                            });
+                    }}
+                    >
+                    Get Image
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => { canvasRef.current.clearCanvas()} }>
+                    Clear drawing
+                </Button>
+            </div>
         </div>
     );
 }
