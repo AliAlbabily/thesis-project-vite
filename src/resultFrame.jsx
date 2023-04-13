589848da19e4d846bce00f3d3267d9c59fbe2000
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-04-13 16:32:05 +0200

    Input text is now transported to ResultFrame-page

--- src/resultFrame.jsx
+++ src/resultFrame.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 function resultFrame() {
+    const location = useLocation();
+    const [text, setText] = useState('');
+
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString('en-US');
+
+    useEffect(() => {
+        setText(location.state.text);
+    }, []);
+
     return ( 
-    <div>
-        <h1>Result</h1>
-    </div>);
+        <div>
+            <h1>Date: {formattedDate}</h1>
+            <p>{text}</p>
+        </div>
+    );
 }
 export default resultFrame;
 
