c4cd6310a45de4edb982f15286748cd63d51f7a9
Ali Fadhel <32642133+AliAlbabily@users.noreply.github.com>
2023-04-25 19:26:48 +0200

    Added new changes in Frame1

--- src/frame1.jsx
+++ src/frame1.jsx
@@ -35,29 +35,40 @@ function Frame1() {
             <h1 className="frame1-header">How are you feeling today ?</h1>
             <Grid container rowSpacing={5} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
                 <Grid item xs={6} md={4}>
-                    <Item onClick={() => handleSelectedEmoji("Happy")} className="emoji-Item">
+                    <Item onClick={() => handleSelectedEmoji("Happy")} className="emoji-Item" id="happy">
                         <img src={happyImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} />
+                        <p className="input-type">Happy</p>
                     </Item>
                 </Grid>
                 <Grid item xs={6} md={4}>
-                    <Item onClick={() => handleSelectedEmoji("Suprised")} className="emoji-Item">
-                        <img src={suprisedImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} /></Item>
+                    <Item onClick={() => handleSelectedEmoji("Suprised")} className="emoji-Item" id="surprised">
+                        <img src={suprisedImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} />
+                        <p className="input-type">Suprised</p>
+                    </Item>
                 </Grid>
                 <Grid item xs={6} md={4}>
-                    <Item onClick={() => handleSelectedEmoji("Scared")} className="emoji-Item">
-                        <img src={fearImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} /></Item>
+                    <Item onClick={() => handleSelectedEmoji("Scared")} className="emoji-Item" id="scared">
+                        <img src={fearImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} />
+                        <p className="input-type">Scared</p>
+                    </Item>
                 </Grid>
                 <Grid item xs={6} md={4}>
-                    <Item onClick={() => handleSelectedEmoji("Angry")} className="emoji-Item">
-                        <img src={angryImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} /></Item>
+                    <Item onClick={() => handleSelectedEmoji("Angry")} className="emoji-Item" id="angry">
+                        <img src={angryImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} />
+                        <p className="input-type">Angry</p>
+                    </Item>
                 </Grid>
                 <Grid item xs={6} md={4}>
-                    <Item onClick={() => handleSelectedEmoji("Sad")} className="emoji-Item">
-                        <img src={sadImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} /></Item>
+                    <Item onClick={() => handleSelectedEmoji("Sad")} className="emoji-Item" id="sad">
+                        <img src={sadImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} />
+                        <p className="input-type">Sad</p>
+                    </Item>
                 </Grid>
                 <Grid item xs={6} md={4}>
-                    <Item onClick={() => handleSelectedEmoji("Disgusted")} className="emoji-Item">
-                        <img src={disgustImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} /></Item>
+                    <Item onClick={() => handleSelectedEmoji("Disgusted")} className="emoji-Item" id="disgusted">
+                        <img src={disgustImage} alt="image" style={{ maxWidth: '100%', height: '150px' }} />
+                        <p className="input-type">Disgusted</p>
+                    </Item>
                 </Grid>
             </Grid>
         </Box>
